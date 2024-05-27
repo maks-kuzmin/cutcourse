568a26c57719bb6505e55a78e70c1ae99a935725
maks <kuzminitb@gmail.com>
2024-05-27 22:22:48 +0600

    change pass DB, add methods for Provider

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
     - php artisan key:generate
     - php artisan storage:link
+    - composer require barryvdh/laravel-debugbar --dev
+    - composer require laravel/telescope
+    - php artisan telescope:install
+    - php artisan migrate
 
 
 # Deploy
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,5 +23,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        //Exception, который будет показываться, когда мы пытаемся сделать запрос N+1
+        Model::preventLazyLoading(!app()->isProduction());
+        /*
+        Exception, который будет показываться, когда мы будем пытаться сохранить какие данные в полях,
+        которые не указаны в $fillable
+        */
+        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+
+        /*
+        Если запросы в БД выполняются больше 500 милисекунд, то этот метод оповещает об этом
+        */
+        DB::whenQueryingForLongerThan(500, function (Connection $connection, QueryExecuted $event) {
+            // TODO 3rd lesson
+        });
+
+        // TODO 3rd lesson request cycle
     }
 }
